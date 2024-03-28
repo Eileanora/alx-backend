0d31342bd2898b939a41053acae2645e387dce8e
yasmeen <yasmeenhany14@gmail.com>
2024-03-28 16:49:16 +0200

    tasks 2, 3

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,7 +5,8 @@ from typing import Tuple, List
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    ''' Return a tuple of size two containing a start index and an end index '''
+    ''' Return a tuple of size two containing a \
+        start index and an end index '''
     return ((page - 1) * page_size, page * page_size)
 
 
@@ -31,8 +32,8 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         ''' Return the appropriate page of the dataset '''
 
-        assert(isinstance(page, int) and page > 0)
-        assert(isinstance(page_size, int) and page_size > 0)
+        assert (isinstance(page, int) and page > 0)
+        assert (isinstance(page_size, int) and page_size > 0)
         start, end = index_range(page, page_size)
 
         if start >= len(self.dataset()):
