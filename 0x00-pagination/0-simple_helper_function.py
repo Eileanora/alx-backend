72c1276c61c17deb356ce0cf49809118a77e06f1
yasmeen <yasmeenhany14@gmail.com>
2024-03-28 15:26:40 +0200

    delete old tasks

--- 0x00-pagination/0-simple_helper_function.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-'''Module containing the index_range function'''
-from typing import Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    '''Returns a tuple containing the start and end indexes'''
-    return ((page - 1) * page_size, page * page_size)
